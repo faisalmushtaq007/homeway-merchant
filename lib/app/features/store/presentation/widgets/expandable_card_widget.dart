c25229620d4eabb532d97d2520a0d75be7e18c7f
Prasant Kumar <prasant10050@gmail.com>
2023-08-04 03:26:57 +0530

    WIP

--- lib/app/features/store/presentation/widgets/expandable_card_widget.dart
+++ lib/app/features/store/presentation/widgets/expandable_card_widget.dart
@@ -52,9 +52,17 @@ class _StoreExpandedCardWidgetController<T, S> extends State<StoreExpandedCardWi
                 width: double.infinity,
                 child: _storeMenuAndPreparationType(context),
               ),
-          2: (BuildContext context) => const Text('The value is B!'),
-          3: (BuildContext context) => const Text('The value is B!'),
-          4: (BuildContext context) => const Text('The value is B!'),
+          2: (BuildContext context) => const Text('Not Available'),
+          3: (BuildContext context) => SizedBox(
+                height: 170,
+                width: double.infinity,
+                child: _storeAvailableDrivers(context),
+              ),
+          4: (BuildContext context) => SizedBox(
+                height: 65,
+                width: double.infinity,
+                child: _storeAvailablePaymentMethods(context),
+              ),
         },
         fallbackBuilder: (BuildContext context) => const Offstage(),
       ),
@@ -158,7 +166,7 @@ class _StoreExpandedCardWidgetController<T, S> extends State<StoreExpandedCardWi
                 ),
               );
             },
-            itemCount: 7, //widget.expandableCardInfo.storeEntity.storeWorkingDays.length,
+            itemCount: 7, //widget.expandableCardInfo.storeEntity.storeAvailableFoodTypes.length,
           ),
         ),
         //const AnimatedGap(3, duration: Duration(milliseconds: 200)),
@@ -202,10 +210,139 @@ class _StoreExpandedCardWidgetController<T, S> extends State<StoreExpandedCardWi
                 ),
               );
             },
-            itemCount: 7, //widget.expandableCardInfo.storeEntity.storeWorkingDays.length,
+            itemCount: 7, //widget.expandableCardInfo.storeEntity.storeAvailableFoodPreparationType.length,
           ),
         ),
       ],
+    );
+  }
+
+  Widget _storeAvailableDrivers(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      //padding: EdgeInsetsDirectional.zero,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Card(
+          key: ValueKey(index),
+          margin: const EdgeInsetsDirectional.only(bottom: 16, end: 8, top: 0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusDirectional.circular(6),
+          ),
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsetsDirectional.only(start: 16, end: 16, top: 12, bottom: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ImageHelper(
+                  image: 'assets/svg/online_driver.svg',
+                  /*image: (widget.storeOwnDeliveryPartnerEntity.imageEntity != null && widget.storeOwnDeliveryPartnerEntity.imageEntity!.imagePath.isNotEmpty)
+                      ? widget.storeOwnDeliveryPartnerEntity.imageEntity?.imagePath ?? ''
+                      : (widget.storeOwnDeliveryPartnerEntity.hasOnline)
+                      ? 'assets/svg/online_driver.svg'
+                      : 'assets/svg/offline_driver.svg',*/
+                  // Quality levels for image sampling in [ImageFilter] and [Shader] objects that sample
+                  filterQuality: FilterQuality.high,
+                  // border radius only work with [ImageShape.rounded]
+                  borderRadius: BorderRadiusDirectional.circular(30),
+                  // alignment of image
+                  //alignment: Alignment.center,
+                  // indicates where image will be loaded from, types are [network, asset,file]
+                  imageType: ImageType.svg,
+                  // indicates what shape you would like to be with image [rectangle, oval,circle or none]
+                  imageShape: ImageShape.rectangle,
+                  // image default box fit
+                  boxFit: BoxFit.fill,
+                  width: context.width / 10,
+                  height: context.width / 10,
+                  defaultErrorBuilderColor: Colors.blueGrey,
+                  errorBuilder: const Icon(
+                    Icons.image_not_supported,
+                    size: 10000,
+                  ),
+                  // loader builder widget, default as icon if null
+                  loaderBuilder: const CircularProgressIndicator(),
+                  matchTextDirection: true,
+                ),
+                Text(
+                  'John D',
+                  style: context.titleMedium!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const AnimatedGap(4, duration: Duration(milliseconds: 200)),
+                Text(
+                  '98765005645',
+                  style: context.labelMedium!.copyWith(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const AnimatedGap(4, duration: Duration(milliseconds: 200)),
+                Chip(
+                  labelPadding: const EdgeInsetsDirectional.all(2),
+                  avatar: const SingleStarRating(
+                    rating: 4.2,
+                    starColor: Color.fromRGBO(42, 45, 50, 1),
+                    starSize: 16,
+                  ),
+                  label: Text(
+                    '4.2',
+                    style: context.labelMedium!.copyWith(
+                      color: const Color.fromRGBO(42, 45, 50, 1),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  backgroundColor: const Color.fromRGBO(69, 201, 125, 1),
+                  elevation: 0.0,
+                  padding: const EdgeInsetsDirectional.all(8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusDirectional.circular(20),
+                  ),
+                ),
+                const AnimatedGap(4, duration: Duration(milliseconds: 200)),
+              ],
+            ),
+          ),
+        );
+      },
+      itemCount: 7, //widget.expandableCardInfo.storeEntity.storeOwnDeliveryPartnersInfo.length,
+    );
+  }
+
+  Widget _storeAvailablePaymentMethods(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      //padding: EdgeInsetsDirectional.zero,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsetsDirectional.only(
+            end: 8,
+          ),
+          child: Chip(
+            labelPadding: const EdgeInsetsDirectional.all(2),
+            avatar: Icon(
+              Icons.payment,
+              color: const Color.fromRGBO(69, 201, 125, 1),
+            ),
+            label: Text(
+              'Cash',
+              style: TextStyle(),
+            ),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            //shadowColor: Colors.grey[60],
+            padding: const EdgeInsetsDirectional.all(8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusDirectional.circular(0),
+            ),
+          ),
+        );
+      },
+      itemCount: 4, //widget.expandableCardInfo.storeEntity.storeAcceptedPaymentModes.length,
     );
   }
 
@@ -309,5 +446,68 @@ class ExpandableCardInfo<T, S> {
       storeEntity: storeEntity ?? this.storeEntity,
       secondaryData: secondaryData ?? this.secondaryData,
     );
+  }
+}
+
+typedef RatingTapCallback = void Function(double rating);
+
+class SingleStarRating extends StatefulWidget {
+  const SingleStarRating({
+    super.key,
+    this.length = 1,
+    this.rating = 0,
+    this.between = 0.0,
+    this.starSize = 20.0,
+    this.color,
+    this.onRaitingTap,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.starColor = const Color.fromRGBO(69, 201, 125, 1),
+  });
+
+  final int length;
+  final double rating;
+  final double between;
+  final double starSize;
+  final RatingTapCallback? onRaitingTap;
+  final Color? color;
+  final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
+  final Color? starColor;
+
+  @override
+  State<SingleStarRating> createState() => _SingleStarRatingState();
+}
+
+class _SingleStarRatingState extends State<SingleStarRating> {
+  late Widget startRateIcon;
+  @override
+  void initState() {
+    super.initState();
+    calculateRate();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  void calculateRate() {
+    if (widget.rating == 0) {
+      startRateIcon = Icon(Icons.star_border, color: widget.starColor, size: widget.starSize);
+    } else {
+      final double value = widget.rating - widget.rating.truncate();
+      if (value == 0) {
+        startRateIcon = Icon(Icons.star, color: widget.starColor, size: widget.starSize);
+      } else if (value > 0 && value < 1) {
+        startRateIcon = Icon(Icons.star_half, color: widget.starColor, size: widget.starSize);
+      }
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return startRateIcon;
   }
 }
